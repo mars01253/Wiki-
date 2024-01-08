944411d4b44ef140ec3339517354d638a8536062
mars01253 <mauadtoto001@gmail.com>
2024-01-08 09:47:10 +0100

    config

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost') ;
+define('DB_USER', 'root') ;
+define('DB_PASS', '') ;
+define('DB_NAME', 'Wiki') ;
+define('APPROOT', dirname(dirname(__FILE__))) ;
+define('URLROOT', 'http://localhost/Wiki-/') ;
